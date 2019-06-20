b82c6cd770f5f5636058c6e5e7ff617bddd67c99
Calvin Shin <calvin.shin@gmail.com>
2019-06-19 19:52:30 -0700

    Updated the connection file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,26 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "freespirits_db"
-});
+var password;
+try {
+    password = require("../../password")
+} catch (e) {
+    if(e.code !== 'MODULE_NOT_FOUND') {
+        throw e;
+    }
+    password = "password"
+}
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: password,
+        database: "freespirits_db"
+    });    
+}
 
 connection.connect(function(err) {
     if (err) {
