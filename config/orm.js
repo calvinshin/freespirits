4cc28d7807586bf9b6d1fddef279044d01b251ec
Calvin Shin <calvin.shin@gmail.com>
2019-06-20 23:57:48 -0700

    ORM is functional for .Read

--- config/orm.js
+++ config/orm.js
@@ -6,22 +6,27 @@ var orm = {
     // Read item
     Read : function(table, displayFunction, column, parameter) {
         // Create the querystring based on what values are returned from Read
+        // Note for the functions below that column and parameter are optional fields. 
         if(column && parameter) {
-            var queryString = "SELECT * FROM ? WHERE ? = ?";
-            connection.query(queryString, [table, column, parameter], function(err, result) {
+            console.log("both exist");
+            var queryString = "SELECT * FROM " + table + " WHERE " + column + " = ?;";
+            connection.query(queryString, [parameter], function(err, result) {
                 if (err) throw err;
                 displayFunction(result);
             });
         }
         else if(column) {
-            var queryString = "SELECT * FROM ? WHERE ? IS NOT NULL";
-            connection.query(queryString, [table, column], function(err, result) {
+            console.log("one exists");
+            var queryString = "SELECT * FROM " + table + " IS NOT NULL;";
+            connection.query(queryString, [column], function(err, result) {
                 if (err) throw err;
                 displayFunction(result);
             });
         }
         else {
-            var queryString = "SELECT * FROM ?"
+            console.log("none exist");
+            var queryString = "SELECT * FROM " + table + ";";
+            console.log(queryString);
             connection.query(queryString, table, function(err, result) {
                 if (err) throw err;
                 displayFunction(result);
